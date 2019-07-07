848571b013362622dc8e9aec389a8f8cd0e5f5bc
Leo H <Testichon@gmail.com>
2019-07-08 01:19:04 +0200

    Add FontAwesome in Atomic design project
    - Possible to make icons with the font awesome library
    - Minor changes in Atomic/atoms

--- Atomic-Design/src/app/app.component.ts
+++ Atomic-Design/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faCoffee, faAngry } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Atomic-Design';
+  faCoffee = faCoffee;
+  faAngry = faAngry;
+
+  constructor() {
+
+  }
 }
