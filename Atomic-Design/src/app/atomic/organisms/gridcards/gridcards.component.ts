a9cf51486dbb1a46e728f853ed49901c0cae1649
Leo H <Testichon@gmail.com>
2019-07-19 19:51:00 +0200

    add a footer, change the icon structure and working on a navbar
    - possible with a switch case to change the icon
    - red footer with header, 3 icons and a paragraph

--- Atomic-Design/src/app/atomic/organisms/gridcards/gridcards.component.ts
+++ Atomic-Design/src/app/atomic/organisms/gridcards/gridcards.component.ts
@@ -6,17 +6,12 @@ import { Component, OnInit, OnChanges, Input } from '@angular/core';
   styleUrls: ['./gridcards.component.css']
 })
 export class GridcardsComponent implements OnInit, OnChanges {
-  @Input() style: string;
-
-  gridcard: string;
-
   constructor() { }
 
   ngOnInit() {
   }
 
   ngOnChanges() {
-   this.gridcard = this.style ? 'style-' + this.style : 'direction';
   }
 
 }
