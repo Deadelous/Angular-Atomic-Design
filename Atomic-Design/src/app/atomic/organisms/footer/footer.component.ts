c393b2c8a1d8ea4f2bf1db64c660345d16164b86
Leo H <Testichon@gmail.com>
2019-07-20 04:08:14 +0200

    footer color possible to change
    - add ngClass "footerColor" in footer.component.html

--- Atomic-Design/src/app/atomic/organisms/footer/footer.component.ts
+++ Atomic-Design/src/app/atomic/organisms/footer/footer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'atomic-o-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnChanges {
+  @Input() color: string;
 
+
+  footerColor: string;
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngOnChanges() {
+   this.footerColor = this.color ? 'backgroundcolor-' + this.color : 'black';
+  }
+
 
 }
